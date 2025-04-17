e7b6678b5fdf6560741e199e4534d5d01f481679
yukkon <tuktuk99@gmail.com>
2025-04-17 12:40:12 +0300

    Get top module

---
+++ exports/GetTop.js
@@ -0,0 +1,37 @@
+export async function getTop() {
+  let a = await Send({
+    calls: [
+      {
+        name: "topGet",
+        args: { type: "arena", extraId: 0 },
+        ident: "group_1_body",
+      },
+    ],
+  }).then((r) =>
+    r.results[0].result.response.top.map((place) =>
+      place.heroes.map((hero) => hero.id)
+    )
+  );
+  let a2 = a
+    .flat()
+    .reduce((acc, val) => (acc[val] ? ++acc[val] : (acc[val] = 1), acc), {});
+
+  //топ героев
+  /*
+    Object.entries(a2)
+      .filter(([id, _]) => id < 100)
+      .sort(([a_id, a_count], [b_id, b_count]) => b_count - a_count)
+      .forEach(([id, count]) =>
+        console.log(`${cheats.translate(`LIB_HERO_NAME_${id}`)} - ${count}`)
+      );
+
+    //топ петов
+    Object.entries(a2)
+      .filter(([id, _]) => id > 100 && id < 7000)
+      .sort(([a_id, a_count], [b_id, b_count]) => b_count - a_count)
+      .forEach(([id, count]) =>
+        console.log(`${cheats.translate(`LIB_HERO_NAME_${id}`)} - ${count}`)
+      );
+    */
+  return a2;
+}
